c6e7cc1484973b9943483724ed54757650f216b5
Simon <vanderweelesimon@gmail.com>
2020-01-31 19:35:08 +0100

    Merged game mode into master

---
+++ Client/ClientGameModes/bin/Debug/netcoreapp3.1/GameModeEvents.cs
@@ -0,0 +1,20 @@
+﻿using RAGE;
+using RAGE.Elements;
+using System;
+
+namespace ClientGameModes
+{
+    public class GameModeEvents : Events.Script
+    {
+        public GameModeEvents()
+        {
+            Events.Add("TriggerVehicleExplosion", BlowUpVehicle);
+        }
+
+        private void BlowUpVehicle(object[] args)
+        {
+            int vehicle = RAGE.Game.Player.GetPlayersLastVehicle();
+            RAGE.Game.Vehicle.ExplodeVehicle(vehicle, true, false);
+        }
+    }
+}
